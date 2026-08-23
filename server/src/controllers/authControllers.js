2c24a1a746f656d54bf0a8a6bec03f03bc1509bd
ThePrakhar15 <prakharsharan15@gmail.com>
2026-08-23 22:42:28 +0530

    Implement JWT authentication

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const registerUser = async( req, res) =>{
 try{
@@ -34,4 +35,46 @@ return res.status(201).json({
 
 };
 
-export default registerUser; 
+const loginUser = async (req , res) =>{
+    try{
+    const {email, password } = req.body;
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        return res.status(400).json({
+         message: "Invalid email or password",   
+        });
+    }
+
+    const isMatch = await bcrypt.compare(password,user.password);
+     
+    if(!isMatch){
+        return res.status(400).json({
+            message: "Invalid email or password",
+        });
+    }
+
+    const token = jwt.sign(
+        {id: user._id},
+        process.env.JWT_SECRET,
+        { expiresIn: "7d"}
+    );
+
+    return res.status(200).json({
+        message:"login succesful",
+        token,
+        user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+        },
+    })
+} catch (error){
+return res.status(500).json({
+    message: "Internal Server Error",
+});
+}
+};
+
+export {registerUser , loginUser}; 
