f97b5dd6221cfd8435b6a76804253cebf5f1c716
ThePrakhar15 <prakharsharan15@gmail.com>
2026-08-25 23:37:10 +0530

    Add update note API

--- server/src/controllers/noteControllers.js
+++ server/src/controllers/noteControllers.js
@@ -35,5 +35,40 @@ const notes = await Note.find({
         });
     }
 }
+const updateNote = async (req , res) =>{
+    try{
+     const {title , content } = req.body;
 
-export { createNote , getNotes};
+     const noteId = req.params.id;
+
+     const note = await Note.findById(noteId);
+     
+     if(!note){
+        return res.status(404).json({
+            message:"Note not found",
+        })
+     }
+     
+        if(note.user.toString() !== req.user._id.toString()){
+            return res.status(403).json({
+                message:"Forbidden",
+            })
+        }
+
+        note.title = title;
+        note.content = content;
+
+        await note.save();
+
+        return res.status(200).json({
+            message: "Note updated successfully",
+            note,
+        });
+
+    } catch(error){
+        return res.status(500).json({
+            message:"Internal Server Error",
+        });
+    }
+};
+export { createNote , getNotes , updateNote };
--- server/src/routes/noteRoutes.js
+++ server/src/routes/noteRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createNote , getNotes} from "../controllers/noteControllers.js"
+import {createNote , getNotes , updateNote} from "../controllers/noteControllers.js"
 import authMiddleware from "../middleware/authMiddleware.js"
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post("/",authMiddleware,createNote);
 router.get("/", authMiddleware,getNotes);
+router.put("/:id", authMiddleware , updateNote);
 
 export default router;
