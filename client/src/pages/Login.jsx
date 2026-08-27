7cf9ca1d334e58c9ca8ac9191c9d43b1f547ec82
ThePrakhar15 <prakharsharan15@gmail.com>
2026-08-27 23:35:22 +0530

    feat: implement controlled login from with react state

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 
 function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div>
             <h1>Login</h1>
             <form>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" placeholder="Enter your email" />
+                <input id="email" type="email" placeholder="Enter your email"
+                value={email} onChange={(e) =>{
+                    setEmail(e.target.value)
+                }} />
 
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password" placeholder="enter your password"/>
+                <input id="password" type="password" placeholder="Enter your password" 
+                value={password} onChange={ (e) =>{
+                    setPassword(e.target.value)
+                }}/>
             </form>
         </div>
     )
