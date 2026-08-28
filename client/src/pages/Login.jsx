a446846f3c52def276f91a4aaa07cc85c2e551c5
ThePrakhar15 <prakharsharan15@gmail.com>
2026-08-28 23:14:09 +0530

    Feat: implement login form validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,11 +4,26 @@ function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = () => {
+        if( email === ""){
+            setError("Email is required");
+            return;
+        }
+        if( password ===  ""){
+            setError("Password is required");
+        return;
+    }
+            setError("");
+            console.log(email,password);
+    }
 
     return (
         <div>
             <h1>Login</h1>
-            <form>
+            <p>{error}</p>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
                 <input id="email" type="email" placeholder="Enter your email"
                 value={email} onChange={(e) =>{
@@ -20,6 +35,9 @@ function Login() {
                 value={password} onChange={ (e) =>{
                     setPassword(e.target.value)
                 }}/>
+                <button type="submit">
+                    Login
+                </button>
             </form>
         </div>
     )
