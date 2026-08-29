2170486ab451a2d6af6aca3e2cee769ceb58f733
ThePrakhar15 <prakharsharan15@gmail.com>
2026-08-29 13:26:28 +0530

    feat: add login form validation and API integration

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import api from "../services/api";
 
 function Login() {
 
@@ -6,19 +7,37 @@ function Login() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
-    const handleSubmit = () => {
-        if( email === ""){
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (email.trim() === "") {
             setError("Email is required");
             return;
         }
-        if( password ===  ""){
+        if (password.trim() === "") {
             setError("Password is required");
-        return;
-    }
-            setError("");
-            console.log(email,password);
-    }
+            return;
+        }
+        if (password.length < 8) {
+            setError("Password must be at least 8 characters");
+            return;
+        }
+        if (!email.includes("@")) {
+            setError("please enter a valid email");
+            return;
+        }
+        setError("");
+        
 
+        try {
+            const response = await api.post("/auth/login", {
+                email,
+                password
+            });
+console.log(response.data.message);
+        } catch (error) {
+            setError(error.response.data.message)
+        }
+    }
     return (
         <div>
             <h1>Login</h1>
@@ -26,15 +45,15 @@ function Login() {
             <form onSubmit={handleSubmit}>
                 <label htmlFor="email">Email</label>
                 <input id="email" type="email" placeholder="Enter your email"
-                value={email} onChange={(e) =>{
-                    setEmail(e.target.value)
-                }} />
+                    value={email} onChange={(e) => {
+                        setEmail(e.target.value)
+                    }} />
 
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password" placeholder="Enter your password" 
-                value={password} onChange={ (e) =>{
-                    setPassword(e.target.value)
-                }}/>
+                <input id="password" type="password" placeholder="Enter your password"
+                    value={password} onChange={(e) => {
+                        setPassword(e.target.value)
+                    }} />
                 <button type="submit">
                     Login
                 </button>
